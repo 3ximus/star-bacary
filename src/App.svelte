d6798b248fff3ba1e3c253277a46aa2ab109ae75
eximus <fabio4335@gmail.com>
2023-09-25 00:50:23 -0400

    base setup

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<main>
+  <h1 id="title">Star Bacary</h1>
+  <img src="bacary.png" alt="">
+</main>
+
+<style>
+  #title {
+    position: absolute;
+    right: 81px;
+    top: 160px;
+    font-size: 5em;
+  }
+  img {
+    height: 100vh;
+  }
+</style>
