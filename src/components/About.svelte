b65ad7b2f41d2ac34634dc67173aaa370596c820
eximus <fabio4335@gmail.com>
2023-10-02 22:42:57 -0400

    add videos and split page into components

---
+++ src/components/About.svelte
@@ -0,0 +1,43 @@
+<div class="about">
+  <img src="bacary.png" alt="" />
+  <p class="bio">
+    Avec sa maîtrise parfaite du rythme africain du 21e siècle, à la frontière
+    du mystique, Star Bacary est résolument tourné vers le monde. Bacary né
+    Sonko est originaire du département de Goudomp, près de Ziguinchor, là où
+    coulent les murmures du fleuve Casamance.
+    <br />
+    <br />
+    Très tôt conscientisé aux maux de la société africaine dont le sort incertain
+    de la jeunesse, il ouvre grand les yeux sur les déroutes des politiciens à l'égard
+    de sa génération. Il devient leur porte-voix, d'ouest en est de sa Casamance,
+    et à travers la diaspora.
+    <br />
+    <br />
+    Le destin tragique de ses amis exilés dans de fatales « pirogues cercueils »
+    le pousse à écrire sa première chanson en 2012, à l'âge de 19 ans "Immigration
+    clandestine"/"Toukou toukou yering", sur ceux qui partent sans laisser trace
+    de vie ni de mort.
+    <br />
+    <br />
+    Il fut membre du fameux trio Keremendeong et en composa la chanson éponyme en
+    plus d'un hymne en hommage au lutteur Balla Gueye II, "Lac de guerre II". Ex-footballeur,
+    son énergie et son charme émanent de son charisme scénique. Il a inventé le style
+    afro-djambo et le propage à l'échelle internationale.
+  </p>
+</div>
+
+<style lang="scss">
+  div.about {
+    display: flex;
+    align-items: center;
+    img {
+      height: 100vh;
+      margin-left: 60px;
+    }
+    p.bio {
+      font-size: 1.5em;
+      max-width: 40%;
+      text-align: justify;
+    }
+  }
+</style>
---
+++ src/components/Videos.svelte
@@ -0,0 +1,50 @@
+<div class="videos">
+  <h1>Videos</h1>
+
+  <a href="https://youtube.com/@bacarysonko4852?si=oOifgrtjNjh2pESC">YOUTUBE</a>
+
+  <div class="list">
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/MqA2AAAtwOw?si=w5NrybFDQKl1KlTV"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/7rv8Jjn0M-c?si=rvBXtRvo8NauBLLW"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
+  </div>
+</div>
+
+<style lang="scss">
+  @use "../colors";
+
+  div.videos {
+    background-color: colors.$background_color_dark;
+    color: colors.$background_color;
+    h1 {
+      font-family: Libel;
+      font-size: 4em;
+      letter-spacing: 4px;
+      text-align: center;
+      margin-top: 0px;
+      padding-top: 10px;
+    }
+
+    .list{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 10%;
+    }
+  }
+</style>
